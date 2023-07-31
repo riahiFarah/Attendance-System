9fe78372c2ce0d7cf1c14c91862ace944453e072
farah riahi <113311714+riahiFarah@users.noreply.github.com>
2023-07-31 22:40:19 +0200

    Add files via upload

---
+++ AddDataToDB.py
@@ -0,0 +1,66 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred , {
+    'databaseURL' : "https://faceattendancertdb-default-rtdb.firebaseio.com/"
+})
+
+ref = db.reference('Students')
+
+data = {
+    "1": {
+        "name": "Elon Musk",
+        "major": "Robotics",
+        "starting_year": 2017,
+        "total_attendance": 6,
+        "standing": 6,
+        "year": 4,
+        "last_attendance_time": "2022-12-11 00:54:34"
+    },
+
+    "2": {
+        "name": "Jackie Chan",
+        "major": "Art",
+        "starting_year": 2016,
+        "total_attendance": 5,
+        "standing": 1,
+        "year": 4,
+        "last_attendance_time": "2022-12-11 00:54:34"
+    },
+
+
+    "3": {
+        "name": "Mark Zuckerberg",
+        "major": "Computer Science",
+        "starting_year": 2018,
+        "total_attendance": 6,
+        "standing": 6,
+        "year": 4,
+        "last_attendance_time": "2022-12-11 00:54:34"
+    },
+
+    "4": {
+        "name": "Ons Jabeur",
+        "major": "Sports",
+        "starting_year": 2017,
+        "total_attendance": 6,
+        "standing": 6,
+        "year": 4,
+        "last_attendance_time": "2022-12-11 00:54:34"
+    },
+
+    "5": {
+        "name": "Farah Riahi",
+        "major": "Computer Science",
+        "starting_year": 2017,
+        "total_attendance": 6,
+        "standing": 6,
+        "year": 4,
+        "last_attendance_time": "2022-12-11 00:54:34"
+    }
+}
+
+for key,value in data.items() :
+    ref.child(key).set((value))
