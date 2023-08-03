d85b2338192ad709a786d95988ac3f73911a6da9
farah riahi <113311714+riahiFarah@users.noreply.github.com>
2023-08-03 19:01:11 +0200

    Update EncodeGenerator.py

--- venv/EncodeGenerator.py
+++ venv/EncodeGenerator.py
@@ -1,7 +1,8 @@
 import cv2
+import os
 import face_recognition
 import pickle
-import os
+
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
@@ -13,45 +14,37 @@ firebase_admin.initialize_app(cred, {
     'storageBucket': "faceattendancertdb.appspot.com"
 })
 
-# Get the absolute path to the 'images' folder
-current_directory = os.path.dirname(os.path.abspath(__file__))
-folderPath = os.path.join(current_directory, '..', 'images')
-
+# Importing people images
+folderPath = 'images'
 pathList = os.listdir(folderPath)
 imgList = []
-studentsIds = []
-
+studentIds = []
 for path in pathList:
     imgList.append(cv2.imread(os.path.join(folderPath, path)))
-    studentsIds.append(os.path.splitext(path)[0])
+    studentIds.append(os.path.splitext(path)[0])
 
-# Upload the images to the Firebase Storage bucket
-bucket = storage.bucket()
-for path in pathList:
-    filePath = os.path.join(folderPath, path)
-    blob = bucket.blob(f'images/{path}')
-    blob.upload_from_filename(filePath)
+    fileName = f'{folderPath}/{path}'
+    bucket = storage.bucket()
+    blob = bucket.blob(fileName)
+    blob.upload_from_filename(fileName)
+
 
 def findEncodings(imagesList):
     encodeList = []
     for img in imagesList:
-        # Changing color from BGR:openCv to RGB:fcaeRecog
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
+
     return encodeList
 
-print('Encoding Started ...')
+print("Encoding ...")
 encodeListKnown = findEncodings(imgList)
-encodeListKnownWithIds = [encodeListKnown, studentsIds]
-print('Encoding Complete')
+encodeListKnownWithIds = [encodeListKnown, studentIds]
+print("Encoding Complete")
 
+# Creating a file with pickle
 file = open("EncodeFile.p", 'wb')
 pickle.dump(encodeListKnownWithIds, file)
 file.close()
-print('File Saved')
-
-file = open("EncodeFile.p" , 'wb')
-pickle.dump (encodeListKnownWithIds ,file)
-file.close ()
-print('File Saved')
+print("File Saved!")
