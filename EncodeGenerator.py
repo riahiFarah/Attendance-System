ef0c5699188fe883ddef5a343dae5321eaf61836
farah riahi <113311714+riahiFarah@users.noreply.github.com>
2023-08-03 19:24:26 +0200

    Add files via upload

---
+++ EncodeGenerator.py
@@ -0,0 +1,50 @@
+import cv2
+import os
+import face_recognition
+import pickle
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import storage
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://faceattendancertdb-default-rtdb.firebaseio.com/",
+    'storageBucket': "faceattendancertdb.appspot.com"
+})
+
+# Importing people images
+folderPath = 'images'
+pathList = os.listdir(folderPath)
+imgList = []
+studentIds = []
+for path in pathList:
+    imgList.append(cv2.imread(os.path.join(folderPath, path)))
+    studentIds.append(os.path.splitext(path)[0])
+
+    fileName = f'{folderPath}/{path}'
+    bucket = storage.bucket()
+    blob = bucket.blob(fileName)
+    blob.upload_from_filename(fileName)
+
+
+def findEncodings(imagesList):
+    encodeList = []
+    for img in imagesList:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+
+    return encodeList
+
+print("Encoding ...")
+encodeListKnown = findEncodings(imgList)
+encodeListKnownWithIds = [encodeListKnown, studentIds]
+print("Encoding Complete")
+
+# Creating a file with pickle
+file = open("EncodeFile.p", 'wb')
+pickle.dump(encodeListKnownWithIds, file)
+file.close()
+print("File Saved!")
