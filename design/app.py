e2b8a0ea08e44b4e064eb16d7c84e3d46ddc8711
farah riahi <113311714+riahiFarah@users.noreply.github.com>
2023-08-25 21:57:09 +0200

    Add files via upload
    part ui

---
+++ design/app.py
@@ -0,0 +1,145 @@
+import os
+import cv2
+import pickle
+
+import cvzone
+import face_recognition
+import numpy as np
+from datetime import datetime
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import storage
+
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://faceattendancertdb-default-rtdb.firebaseio.com/",
+    'storageBucket': "faceattendancertdb.appspot.com"
+})
+
+bucket = storage.bucket()
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap.set(3, 640)
+cap.set(4, 480)
+
+imgBackground = cv2.imread('ressources/background.png')
+folderModePath = 'ressources/modes'
+modePathList = os.listdir(folderModePath)
+imgModeList = []
+
+for path in modePathList:
+    imgModeList.append(cv2.imread(os.path.join(folderModePath, path)))
+
+# Load the encoding file
+print("Loading Encoded File ...")
+file = open('EncodeFile.p', 'rb')
+encodeListKnownWithIds = pickle.load(file)
+file.close()
+encodeListKnown, peopleIds = encodeListKnownWithIds
+print("Encoded File Loaded!")
+
+modeType = 0
+counter = 0
+id = -1
+imgPeople = []
+
+while True:
+    success, img = cap.read()
+
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    faceCurFrame = face_recognition.face_locations(imgS)
+    encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
+
+    imgBackground[162:162 + 480, 55:55 + 640] = img
+    imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
+
+    if faceCurFrame:
+
+        for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
+            matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+
+            matchIndex = np.argmin(faceDis)
+            if matches[matchIndex]:
+                y1, x2, y2, x1 = faceLoc
+                y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+                bbox = 55+x1, 162+y1, x2-x1, y2-y1
+                imgBackground = cvzone.cornerRect(imgBackground, bbox, rt=0)
+                id = peopleIds[matchIndex]
+                if counter == 0:
+                    counter = 1
+                    modeType = 1
+
+        if counter != 0:
+            if counter == 1:
+                # Getting the data
+                peopleInfo = db.reference(f'Students/{id}').get()
+
+                # Getting the Image from Firebase storage
+                blob = bucket.get_blob(f'images/{id}.png')
+                array = np.frombuffer(blob.download_as_string(), np.uint8)
+                imgPeople = cv2.imdecode(array, cv2.COLOR_BGRA2BGR)
+
+                # Update attendance
+                datetimeObject = datetime.strptime(peopleInfo['last_attendance_time'], "%Y-%m-%d %H:%M:%S")
+                secondsElapsed = (datetime.now()-datetimeObject).total_seconds()
+
+                if secondsElapsed > 30:
+                    ref = db.reference(f'Students/{id}')
+                    peopleInfo['total_attendance'] += 1
+                    ref.child('total_attendance').set(peopleInfo['total_attendance'])
+                    ref.child('last_attendance_time').set(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                else:
+                    modeType = 3
+                    counter = 0
+                    imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
+
+            if modeType != 3:
+
+                if 10 < counter < 20:
+                    modeType = 2
+
+                imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
+
+                if counter <= 10:
+                    cv2.putText(imgBackground, str(peopleInfo['total_attendance']), (861, 125),
+                                cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1)
+                    cv2.putText(imgBackground, str(peopleInfo['major']), (1006, 550),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.4, (255, 255, 255), 1)
+                    cv2.putText(imgBackground, str(id), (1006, 493),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+                    cv2.putText(imgBackground, str(peopleInfo['standing']), (910, 625),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
+                    cv2.putText(imgBackground, str(peopleInfo['year']), (1025, 625),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
+                    cv2.putText(imgBackground, str(peopleInfo['starting_year']), (1125, 625),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
+
+                    # Center automatically the name
+                    (w, h), _ = cv2.getTextSize(peopleInfo['name'], cv2.FONT_HERSHEY_COMPLEX, 1, 1)
+                    offset = (414 - w)//2
+                    cv2.putText(imgBackground, str(peopleInfo['name']), (808 + offset, 445),
+                                cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1)
+
+                    imgBackground[175:175+216, 909:909+216] = imgPeople
+
+                counter += 1
+
+                if counter >= 20:
+                    counter = 0
+                    modeType = 0
+                    peopleInfo = []
+                    imgPeople = []
+                    imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
+    else:
+        modeType = 0
+        counter = 0
+
+    # cv2.imshow("Webcam", img)
+    cv2.imshow("Face Attendance", imgBackground)
+    cv2.waitKey(1)
